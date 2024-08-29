10c1b242f5a016c3d499e7fc2c22e96731401b5c
BlertaMavriqi <mavriqiblerta@gmail.com>
2024-08-29 16:20:38 +0200

    modul27.2

---
+++ modul27.2/app.js
@@ -0,0 +1,27 @@
+//do while loop
+
+var i = 0;
+
+do{
+    //i++
+    i = i+1
+    console.log(i)
+}while(i <5)
+
+console.log("-----------")
+
+// while loop
+var j = 0;
+while(j<5){
+    j++
+    console.log(j)
+}
+
+console.log("-----------")
+
+// for loop
+
+for(var i = 0; i<6 ; i++){
+    console.log("value of i is:" + i)
+}
+console.log("-----------")
