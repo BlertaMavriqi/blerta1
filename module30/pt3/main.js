70e38eb81ef288adb515582ae9207c382fef1958
BlertaMavriqi <mavriqiblerta@gmail.com>
2024-09-03 18:17:31 +0200

    module 31

--- module30/pt3/main.js
+++ module30/pt3/main.js
@@ -4,16 +4,16 @@ var btnGuess = document.getElementById ("btnGuess");
 var count = 1;
 var guessNumber = false;
 btnGuess.onclick = function(){
-    count= 1;
+    count = 1;
     guessNumber = false;
+    var random = Math.floor(Math.random()*6);
 
-    while (guessNumber== false){
-        var random = Math.floor(Math.random()*6);
+    while (guessNumber == false){
         console.log(random)
 
         if (nrSelect.value == random){
             guessNumber = true;
-            console.log("Guessed within :" +count + "times")
+            console.log("Guessed within :" + count + "times")
         }
         
         else{
