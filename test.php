53dc7e2e9b459334daf2c5312e3d9dd7d2530b20
BlertaMavriqi <mavriqiblerta@gmail.com>
2024-09-20 18:44:55 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+   //echo "hello world";
+   //$school = "digital school";
+
+   //echo "I love $school";
+
+
+   //$x = 120;
+   //$y = 50;
+   //echo $x + $y; 
+   //echo $x - $y; 
+   //echo $x / $y; 
+   //echo $x * $y; 
+   //echo $x % $y; 
+
+   //$a = "digital";
+   //$b = "school";
+   //$c = $a.$b ;
+
+   //echo "$c \n";
+
+   //$the_string = 'digital school';
+   //echo strlen($the_string);
+   //echo str_word_count($the_string);
+
+   //$programming = 'programming is not cool';
+   //echo str_replace("not", "very", $programming);
+
+   $the_string = 'Programming';
+   echo strrev($the_string);
+?>
+</body>
+</html>
